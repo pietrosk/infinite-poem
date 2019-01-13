20b21de0955f7badb3674d33af9e861477c46831
Peter Skoda <skodap@gmail.com>
2019-01-13 14:52:32 +0100

    Updated GET verses endpoint

--- src/Stores/Reactive.ts
+++ src/Stores/Reactive.ts
@@ -2,17 +2,21 @@ import { autorun } from "mobx";
 import languageStore from "./LanguageStore";
 import poemStore from "./PoemStore";
 
+interface ApiVerseModel {
+  text: string,
+}
+
 export const runReactiveStores = () => {
   var poemLoader = autorun(() => {
     console.log('now load for new language', languageStore.currentLanguage);
 
-    const url = `https://infinitepoemv1.azurewebsites.net/api/verse?language=${languageStore.currentLanguage}`;
+    const url = `https://infinitepoemv1.azurewebsites.net/api/verses/${languageStore.currentLanguage}`;
     fetch(url)
       .then(response => response.json())
       .then(response => { 
         console.log('got result:', response);
-        return response as string[];
+        return response as ApiVerseModel[];
       })
-      .then(response => poemStore.verses = response);
+      .then(response => poemStore.verses = response.map(v => v.text));
   });
 }
